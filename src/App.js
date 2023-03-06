6fd7e5e002e70c31cc8643e24c2f16f4d72a6032
QodirovDoston <qodirovdoston806@gmail.com>
2023-03-06 19:52:31 +0500

    zcxcc

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import axios from 'axios'
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
-const url= `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
 
-  const searchLocation = (event) => {
+const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=189271b827844bff7388350c44848615&units=metric`
+  
+const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
         setData(response.data)
@@ -37,29 +38,26 @@ const url= `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=
             {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data.weather ? <p>{data.weather[0].main  } </p> : null}
           </div>
         </div>
 
         {data.name !== undefined &&
           <div className="bottom">
             <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C   🌩</p> : null}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              {data.main ? <p className='bold'>{data.main.humidity}% ☔️ </p> : null}
               <p>Humidity</p>
             </div>
             <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH 🍃</p> : null}
               <p>Shamaol tezligi</p>
             </div>
           </div>
         }
-
-
-
       </div>
     </div>
   );
